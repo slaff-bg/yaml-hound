da4d35923a2380ab00e737f0d04c6b905a341a05
Slaff <slaff.bg@gmail.com>
2023-02-07 21:04:25 +0200

    The FootprintSniffer function is completed. The tests as well.

--- footprint-sniffer.go
+++ footprint-sniffer.go
@@ -4,24 +4,25 @@ import (
 	"fmt"
 )
 
-// ti - trace index (
+// The function traverses the parsed YAML file and looks for a match against the
+// supplied key. Returns the first match found.
+// If more than one key is passed, the function looks for an exact match of the
+// sequence in the YAML tree.
 //
-//	true - trace indepth;
-//	false - trace for succes only first lvl keys (yum);
+// yum - parsed YAML file
+// yfp - the slice contains the search key. The part contains the search key or
+// the exact match of the sequence keys (if more than one key is supplied).
+// fid - whether to follow the index in-depth or not. (
+//
+//	true - trace in-depth
+//	false - only trace for a matching key on the first YAML level (does not extend to sub-levels)
 //
 // )
-func (y *YAMLTracer) footprintSniffer(yum map[string]interface{}, yfp []string, ti bool) (map[string]interface{}, bool) {
+func (y *YAMLTracer) footprintSniffer(yum map[string]interface{}, yfp []string, fid bool) (map[string]interface{}, bool) {
+	y.Found = false
 fOut:
 	for _, yfpV := range yfp {
 		for yumK, yumV := range yum {
-			fmt.Printf("\n::: yumK (%v) :::\n", yumK)
-			// 1. The first key searched matches the YAML crawl key.
-			// 		1.1. We check for the type of value.
-			//          1.1.1. The value is different from the MAP (we return it because it is assumed that it's the value searched for).
-			//          1.1.2. The value is MAP (do recursion).
-			//              -- remove one element from the searched keys, and if there is another key, we continue cycling.
-
-			//  1.1.1. Finds a first-level key.
 			if yfpV == yumK {
 				if fmt.Sprintf("%T", yumV) != "map[string]interface {}" {
 					y.Caught, y.Found = yumV, true
@@ -29,55 +30,20 @@ fOut:
 				}
 
 				if len(yfp)-1 > 0 {
-					if _, ok := y.footprintSniffer(yumV.(map[string]interface{}), yfp[1:], false); ok {
-						y.Found = true
-					}
+					y.footprintSniffer(yumV.(map[string]interface{}), yfp[1:], false)
 				}
 
 				break fOut
 			}
 
-			//  1.1.2.
-			// if _, ok := y.footprintSniffer(yumV.(map[string]interface{}), yfp, true); ok {
-			// 	y.Found = true
-			// 	break fOut
-			// }
-
-			// if fmt.Sprintf("%T", yumV) == "map[string]interface {}" {
-			// if yumK == yfpV {
-
-			// }
-			// res, ok := y.footprintSniffer(yumV.(map[string]interface{}), yfp[0:])
-			// if ok {
-			// 	break
-			// 	return res, true
-			// }
-			// return map[string]interface{}{}, false
-			// continue
-			// }
-
-			// if yumK == yfpV {
-			// 	if yfpK+1 == len(yfp) {
-			// 		y.Caught = yumV
-			// 		y.Found = true
-			// 		return map[string]interface{}{}, true
-			// 	}
-			// }
-
-			// return map[string]interface{}{}, false
-
-			// if yumK == yfpV {
-			// 	if fmt.Sprintf("%T", yumV) == "map[string]interface {}" {
-			// 		//
-			// 	}
-
-			// 	if yfpK+1 == len(yfp) {
-			// 		y.Caught = yumV
-			// 		y.Found = true
-			// 		// return map[string]interface{}{}, true
-			// 	}
-			// }
+			if fid && fmt.Sprintf("%T", yumV) == "map[string]interface {}" {
+				if _, ok := y.footprintSniffer(yumV.(map[string]interface{}), yfp, true); ok {
+					break fOut
+				}
+			}
 		}
+
+		break fOut
 	}
 
 	return map[string]interface{}{}, y.Found
--- yaml-hound.go
+++ yaml-hound.go
@@ -2,9 +2,18 @@ package yamlhound
 
 import (
 	"errors"
-	"fmt"
 )
 
+// Struct properties UnmYAML and Footprints must be defined before the function
+// is executed.
+//
+// - UnmYAML: unmarshalled YAML file
+// - Footprints: the search key or strict sequence of search keys
+//
+// The response after the function is executed will be found in the following
+// properties:
+// - Caught: contains the value of the searched key after processing
+// - Found: whether the key was found or not
 type YAMLTracer struct {
 	UnmYAML    map[string]interface{}
 	Footprints []string
@@ -12,6 +21,10 @@ type YAMLTracer struct {
 	Found      bool
 }
 
+// The function traverses the parsed YAML file and looks for a match against the
+// supplied key. Returns the first match found.
+// If more than one key is passed, the function looks for an exact match of the
+// sequence in the YAML tree.
 func (y *YAMLTracer) FootprintSniffer() error {
 	fl := len(y.Footprints)
 	if fl < 1 {
@@ -19,10 +32,7 @@ func (y *YAMLTracer) FootprintSniffer() error {
 		// submitted.
 		return errors.New("no traces to follow.")
 	}
-
-	// _, _ = y.footprintSniffer(y.UnmYAML, y.Footprints)
-
-	fmt.Println("\n::::::::::", y.Caught, "::::::::::")
+	_, _ = y.footprintSniffer(y.UnmYAML, y.Footprints, true)
 
 	return nil
 }
